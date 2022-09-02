e84ec7fe7a5294a38bf985df1c09cd3aabfd3f10
Gilmielle <bizarre_princes@mail.ru>
2022-09-02 11:31:34 +0500

    helmet CSP set false, build script corrected

--- src/server/server.js
+++ src/server/server.js
@@ -7,16 +7,14 @@ import compression from 'compression';
 import helmet from 'helmet';
 
 const PORT = process.env.PORT || 3000;
-const IS_DEV = process.env.NODE_ENV !== 'production';
 const DOMAIN = process.env.DOMAIN;
 
 const app = express();
-if (!IS_DEV) {
-  app.use(compression());
-  app.use(helmet({
-    // contentSecurityPolicy: false,
-  }));
-}
+
+app.use(compression());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 
 app.use('/static', express.static('./dist/client'));
 
